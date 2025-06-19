6af6d0266ce7c49773f482214bc0cb6edb6f26cb
蒼之 中谷 <C3349@oic.jp>
2025-06-19 16:17:09 +0900

    EmployeeCreatorForm.cs,EmployeeCreatorForm.DesignerのCreateGuestEventにエラーあり

--- OOProjectBasedLeaning/EmployeeCreatorForm.cs
+++ OOProjectBasedLeaning/EmployeeCreatorForm.cs
@@ -17,7 +17,7 @@ namespace OOProjectBasedLeaning
         {
             InitializeComponent();
 
-            // ボタンの初期化とイベント登録
+            // 従業員作成ボタンの初期化とイベント登録
             createButton = new Button
             {
                 Text = "従業員を作成",
@@ -29,37 +29,12 @@ namespace OOProjectBasedLeaning
             Controls.Add(createButton);
         }
 
-        /// <summary>
-        /// 従業員作成イベント。新しい従業員パネルをフォームに追加します。
-        /// </summary>
-        private void CreateGuestEvent(object sender, EventArgs e)
-        {
-            var newEmployee = CreateEmployee();
-
-            var panel = new EmployeePanel(newEmployee)
-            {
-                Location = new Point(10, 50 + (Controls.Count - 1) * 60),
-                Width = 300
-            };
-
-            Controls.Add(panel);
-        }
-
-        /// <summary>
-        /// 新しい従業員インスタンスを生成します。
-        /// </summary>
-        private Employee CreateEmployee()
-        {
-            employeeId++;
-            return new EmployeeModel(employeeId, "Employee" + employeeId);
-        }
-
-        /// <summary>
-        /// フォームロード時の初期化処理。
-        /// </summary>
+        // フォームロード時の初期化処理
         private void EmployeeCreatorForm_Load(object sender, EventArgs e)
         {
             this.Text = "従業員作成フォーム";
+
         }
+
     }
 }
