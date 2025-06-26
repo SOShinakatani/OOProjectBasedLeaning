b03c5c31db2946d9aa15e572ff3a82c7c3f83c79
蒼之 中谷 <C3349@oic.jp>
2025-06-26 14:03:51 +0900

    従業員作成フォーム更新

--- OOProjectBasedLeaning/EmployeeCreatorForm.Designer.cs
+++ OOProjectBasedLeaning/EmployeeCreatorForm.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace OOProjectBasedLeaning
+﻿
+namespace OOProjectBasedLeaning
 {
     partial class EmployeeCreatorForm
     {
@@ -26,21 +27,34 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
+        private TextBox nameTextBox;
+
         private void InitializeComponent()
         {
-            button1 = new Button();
+            //button1 = new Button();
+            nameTextBox = new TextBox(); // ← 追加
             SuspendLayout();
+
+            // 
+            // nameTextBox
+            // 
+            nameTextBox.Location = new Point(50, 50);
+            nameTextBox.Size = new Size(300, 27);
+            nameTextBox.Name = "nameTextBox";
+            nameTextBox.TabIndex = 1;
+
             // 
             // button1
             // 
-            button1.Location = new Point(491, 16);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(102, 41);
-            button1.TabIndex = 0;
-            button1.Text = "従業員の作成";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += CreateGuestEvent;
+            //button1.Location = new Point(491, 16);
+            //button1.Margin = new Padding(3, 4, 3, 4);
+            //button1.Name = "button1";
+            //button1.Size = new Size(102, 41);
+            //button1.TabIndex = 0;
+            //button1.Text = "従業員の作成";
+            //button1.UseVisualStyleBackColor = true;
+            //button1.Click += CreateGuestEvent;
+
             // 
             // EmployeeCreatorForm
             // 
@@ -48,12 +62,22 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(607, 419);
             Controls.Add(button1);
+            Controls.Add(nameTextBox); // ← 追加
             Margin = new Padding(3, 4, 3, 4);
             Name = "EmployeeCreatorForm";
             Text = "EmployeeCreatorForm";
             Load += EmployeeCreatorForm_Load;
             ResumeLayout(false);
+            PerformLayout(); // ← Layout更新
         }
+
+
+        private void CreateGuestEvent(object sender, EventArgs e)
+        {
+            string employeeName = nameTextBox.Text;
+            MessageBox.Show($"従業員「{employeeName}」を作成しました。");
+        }
+
 
         #endregion
 
