4df2574992dc3e2eb0ec045f0bb17371bb671ed1
蒼之 中谷 <C3349@oic.jp>
2025-06-26 16:07:01 +0900

    結局元通り

--- OOProjectBasedLeaning/EmployeeCreatorForm.cs
+++ OOProjectBasedLeaning/EmployeeCreatorForm.cs
@@ -11,23 +11,23 @@ namespace OOProjectBasedLeaning
     public partial class EmployeeCreatorForm : Form
     {
         private int employeeId = 10000;
-        private Button createButton;
+        //private Button createButton;
 
-        public EmployeeCreatorForm()
-        {
-            InitializeComponent();
+        //public EmployeeCreatorForm()
+        //{
+        //    InitializeComponent();
 
-            // 従業員作成ボタンの初期化とイベント登録
-            createButton = new Button
-            {
-                Text = "従業員を作成",
-                Location = new Point(10, 10),
-                Size = new Size(120, 30)
-            };
-            createButton.Click += CreateGuestEvent;
+        //    // 従業員作成ボタンの初期化とイベント登録
+        //    createButton = new Button
+        //    {
+        //        Text = "従業員を作成",
+        //        Location = new Point(10, 10),
+        //        Size = new Size(120, 30)
+        //    };
+        //    createButton.Click += CreateGuestEvent;
 
-            Controls.Add(createButton);
-        }
+        //    Controls.Add(createButton);
+        //}
 
         // フォームロード時の初期化処理
         private void EmployeeCreatorForm_Load(object sender, EventArgs e)
--- OOProjectBasedLeaning/HomeForm.cs
+++ OOProjectBasedLeaning/HomeForm.cs
@@ -1,24 +1,35 @@
-using OOProjectBasedLeaning;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using OOProjectBasedLeaning.Models;
+
 namespace OOProjectBasedLeaning
 {
-
     public partial class HomeForm : Form
     {
         private TimeTrackerModel tracker;
         private TimeTrackerPanel panel;
+        private ComboBox employeeComboBox;
 
         public HomeForm(TimeTrackerModel tracker)
         {
-
-            InitializeComponent();
-
             this.tracker = tracker;
+            InitializeComponent();
 
             var company = new CompanyModel("サンプル会社");
             company.AddTimeTracker(tracker);
 
             panel = new TimeTrackerPanel(tracker, company);
             this.Controls.Add(panel);
+
+            employeeComboBox = new ComboBox
+            {
+                Location = new Point(10, 220),
+                Size = new Size(200, 30)
+            };
+            Controls.Add(employeeComboBox);
+
+            this.Text = "ホームフォーム";
         }
 
         public void SetLogHandler(EventHandler<string> handler)
@@ -26,10 +37,13 @@ namespace OOProjectBasedLeaning
             panel.LogUpdated += handler;
         }
 
-        private void HomeForm_Load(object sender, EventArgs e)
+        public void RefreshEmployeeList()
         {
-
+            employeeComboBox.Items.Clear();
+            foreach (var emp in EmployeeRepository.GetAll())
+            {
+                employeeComboBox.Items.Add(emp);
+            }
         }
     }
-
 }
