b5861b51a8555d065b85a8235c068edaf541f983
1a10Si <C3160@oic.jp>
2025-06-26 13:29:36 +0900

    出退勤画面に社員の名前を出すのができた

--- OOProjectBasedLeaning/HomeForm.cs
+++ OOProjectBasedLeaning/HomeForm.cs
@@ -9,10 +9,20 @@ namespace OOProjectBasedLeaning
 
             InitializeComponent();
 
-            var timeTracker = new TimeTrackerModel(NullCompany.Instance); // Company の代わりに仮のものを使う
-            var panel = new TimeTrackerPanel(timeTracker);
+            // 1. 本物の会社（CompanyModel）を作る
+            var company = new CompanyModel("サンプル会社");
 
-            this.Controls.Add(panel);
+            // 2. TimeTrackerModel にその会社を渡して作る
+            var timeTracker = new TimeTrackerModel(company);
+
+            // 3. 会社側にも TimeTracker を登録しておく（双方向リンク）
+            company.AddTimeTracker(timeTracker);
+
+            // 4. パネルを作る（companyとtimeTrackerの両方を渡す）
+            var panel = new TimeTrackerPanel(timeTracker, company);
+
+            // 5. パネルをフォームに追加
+            this.Controls.Add(panel); ;
 
         }
 
